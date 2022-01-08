edb529bef5cf7f22e12e66adbcf94ec3d0a94f4e
kosyakoff1 <asondor@salusoptima.com>
2022-01-08 19:23:33 +0700

    Added navigation skeleton for the app

--- app/src/main/java/com/kosyakoff/distancetrackerapp/MainActivity.kt
+++ app/src/main/java/com/kosyakoff/distancetrackerapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.kosyakoff.distancetrackerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
