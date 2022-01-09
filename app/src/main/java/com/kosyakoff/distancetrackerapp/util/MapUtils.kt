aab46ef15a763c39d04119ec4e5843ff2855ccf3
kosyakoff1 <asondor@salusoptima.com>
2022-01-10 06:21:13 +0700

    Added measuring elapsed time of tracking

--- app/src/main/java/com/kosyakoff/distancetrackerapp/util/MapUtils.kt
+++ app/src/main/java/com/kosyakoff/distancetrackerapp/util/MapUtils.kt
@@ -2,6 +2,8 @@ package com.kosyakoff.distancetrackerapp.util
 
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.SphericalUtil
+import java.text.DecimalFormat
 
 object MapUtils {
     fun setCameraPosition(location: LatLng): CameraPosition {
@@ -17,5 +19,15 @@ object MapUtils {
         val minutes = (elapsedTime / (1000 * 60) % 60)
         val hours = (elapsedTime / (1000 * 60 * 60) % 24)
         return "$hours:$minutes:$seconds"
+    }
+
+    fun calculateDistance(locationList: MutableList<LatLng>): String {
+        if (locationList.size > 1) {
+            val meters =
+                SphericalUtil.computeDistanceBetween(locationList.first(), locationList.last())
+            val kilometers = meters / 1000
+            return DecimalFormat("#.##").format(kilometers)
+        }
+        return "0.00"
     }
 }
